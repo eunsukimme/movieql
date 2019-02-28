ff6d29ec91b1846cc7112ee0604a011e18e18ca9
eunsukimme <eunsu.dev@gmail.com>
2019-02-28 17:53:38 +0900

    Move legacy files to test folder and Add yts API and fetch function to get movies from yts

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,14 @@
-import { people, movies, getPersonById, getMovieById, addMovie, deleteMovie } from './db';
+import { getMovies } from './db';
 
 export const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, { id }) => getPersonById(id),
-        movies: () => movies,
-        movie: (_, { id }) => getMovieById(id)
+        //people: () => people,
+        //person: (_, { id }) => getPersonById(id),
+        movies: () => getMovies()
+        //movie: (_, { id }) => getMovieById(id)
     },
-    Mutation: {
-        addMovie: ( _, { name, score }) => addMovie(name, score), 
-        deleteMovie: (_, { id }) => deleteMovie(id)
-    }
+    /*Mutation: {
+        //addMovie: ( _, { name, score }) => addMovie(name, score), 
+        //deleteMovie: (_, { id }) => deleteMovie(id)
+    }*/
 }
