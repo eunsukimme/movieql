ce7ef4df6cc2cc93367f80800cc0348c8942ba16
eunsukimme <eunsu.dev@gmail.com>
2019-03-02 12:19:09 +0900

    Add query string limit and minimum_rating

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,13 +2,6 @@ import { getMovies } from './db';
 
 export const resolvers = {
     Query: {
-        //people: () => people,
-        //person: (_, { id }) => getPersonById(id),
-        movies: () => getMovies()
-        //movie: (_, { id }) => getMovieById(id)
-    },
-    /*Mutation: {
-        //addMovie: ( _, { name, score }) => addMovie(name, score), 
-        //deleteMovie: (_, { id }) => deleteMovie(id)
-    }*/
+        movies: (_, { limit, rating }) => getMovies(limit, rating)
+    }
 }
